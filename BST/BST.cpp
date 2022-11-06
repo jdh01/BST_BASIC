2684d24ec807fcaafb1445c96110192b8140ded5
jdh01 <jdh23@kent.ac.uk>
2022-11-06 17:21:01 +0000

    Basic implementation of a Binary Search Tree

---
+++ BST/BST.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+struct BstNode {
+	int data;
+	BstNode* left;
+	BstNode* right;
+};
+
+BstNode* GetNewNode(int data) {
+	BstNode* newNode = new BstNode();
+	(*newNode).data = data; //or newNode-> = data;
+	(*newNode).left = (*newNode).right = NULL; //intialise left and right to NULL
+	return newNode;
+}
+
+BstNode* Insert(BstNode* root, int data) {
+	if (root == NULL) {
+		root = GetNewNode(data);
+		return root;
+	}
+	else if (data <= root->data) {
+		root->left = Insert(root->left, data);
+	}
+	else {
+		root->right = Insert(root->right, data);
+	}
+	return root;
+}
+
+bool Search(BstNode* root, int data) {
+	if (root == NULL) return false;
+	else if (root->data == data) return true;
+	else if (data <= root->data) return Search(root->left, data);
+	else return Search(root->right, data);
+}
+
+int main()
+{
+	BstNode* root; //pointer to root node
+	root = NULL; //sets the tree as empty
+	Insert(root, 15);
+	Insert(root, 10);
+	Insert(root, 20);
+
+	Search(root, 15);
+}
